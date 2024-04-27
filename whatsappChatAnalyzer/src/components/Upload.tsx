f85ec4e87aa27a9dd193b4ff8825fd7d85ceefe7
priest davos <rajjo1920london@gmail.com>
2024-04-27 23:54:44 +0530

    update upload.tsx 4 Frontend setup for file upload update Upload.tsx component

--- whatsappChatAnalyzer/src/components/Upload.tsx
+++ whatsappChatAnalyzer/src/components/Upload.tsx
@@ -1,25 +1,38 @@
-// src/components/Upload.tsx
-
 import React from 'react';
 import { Upload, Button, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import api from '../api';
 
 const UploadComponent: React.FC = () => {
+
+  const handleUpload = async (file: File) => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+      // console.log(file)
+
+      // Make a POST request to the server to upload the file
+      const response = await api.post('/ChatAnalyzerApi/upload/', formData, {
+        headers: {
+          'Content-Type': 'form-data', // Set content type to multipart/form-data
+        },
+      });
+
+      // Handle successful upload
+      console.log('File uploaded successfully:', response.data);
+      message.success(`${file.name} file uploaded successfully`);
+    } 
+    catch (error) {
+      // Handle upload error
+      console.error('Error uploading file:', error);
+      message.error(`${file.name} file upload failed.`);
+    }
+  };
+
   const props = {
-    name: 'file',
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76', // Replace with your upload URL
-    headers: {
-      authorization: 'authorization-text',
-    },
-    onChange(info: any) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
+    beforeUpload: (file: File) => {
+      handleUpload(file);
+      return false; // Prevent default upload behavior
     },
   };
 
